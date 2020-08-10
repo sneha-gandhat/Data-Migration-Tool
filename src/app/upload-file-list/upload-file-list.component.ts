906ce6352262c31b66bd89ccbcef47427133cde7
Sneha Gandhat <sneha_gandhat@persistent.com>
2020-08-10 11:56:34 +0530

    Added tooltip for table options

--- src/app/upload-file-list/upload-file-list.component.ts
+++ src/app/upload-file-list/upload-file-list.component.ts
@@ -1,6 +1,6 @@
 import { from } from 'rxjs';
 import { File } from './file';
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ViewEncapsulation } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -14,7 +14,8 @@ import * as fileSaver from 'file-saver';
 @Component({
   selector: 'app-upload-file-list',
   templateUrl: './upload-file-list.component.html',
-  styleUrls: ['./upload-file-list.component.css']
+  styleUrls: ['./upload-file-list.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class UploadFileListComponent implements OnInit, AfterViewInit {
@@ -28,6 +29,7 @@ export class UploadFileListComponent implements OnInit, AfterViewInit {
   pipe: DatePipe;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+  //disabled = new FormControl(false);
 
   constructor(private fileService: FileUploadService) {
   }
@@ -41,42 +43,7 @@ export class UploadFileListComponent implements OnInit, AfterViewInit {
     this.getAllFileDetails();
   }
 
-  //Search Filter
-  applyFilter(filterValue: string) {
-    // console.log('filterValue'+filterValue);
-    // console.log(this.filelistdataSource);
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
-    this.filelistdataSource.filter = filterValue;
-  }
-
-  //Date Range Filter
-  filterForm = new FormGroup({
-    fromDate: new FormControl(),
-    toDate: new FormControl(),
-  });
-
-  get fromDate() { return this.filterForm.get('fromDate').value; }
-  get toDate() { return this.filterForm.get('toDate').value; }
-
-  filterByDateRange() {
-    this.pipe = new DatePipe('en');
-    this.filelistdataSource.filterPredicate = (data, filter) => {
-      if (this.fromDate && this.toDate) {
-        return data.uploadedDate >= this.fromDate && data.uploadedDate <= this.toDate;
-      }
-      return true;
-    }
-    this.filelistdataSource.filter = '' + Math.random();
-  }
-
-  //Clear Filters
-  clearFilter() {
-    this.filelistdataSource.filter = '';
-    this.fromSelected = '';
-    this.toSelected = '';
-  }
-
+  //Delete File
   deleteFile(item: any) {
     if (window.confirm('Are sure you want to delete this item ?')) {
       this.fileService.deleteFile(item.fileDeleteUri).subscribe(
@@ -93,6 +60,7 @@ export class UploadFileListComponent implements OnInit, AfterViewInit {
 
   }
 
+  //Download File
   downloadFile(item: any) {
     this.fileService.downloadFile(item.fileDownloadUri).subscribe(response => {
       let blob: any = new Blob([response], { type: 'text/json; charset=utf-8' });
@@ -102,6 +70,7 @@ export class UploadFileListComponent implements OnInit, AfterViewInit {
       () => console.info('File downloaded successfully');
   }
 
+  //Render all files data in table
   getAllFileDetails() {
     this.fileService.getUploadedFilesDeatils().subscribe(
       data => {
