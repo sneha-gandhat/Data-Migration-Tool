229ca14a4a4b62a1d5a63e5d8a35d8ba4ddcc9fe
Sneha Gandhat <sneha_gandhat@persistent.com>
2020-08-10 12:31:39 +0530

    Added Setting component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
-import { LoginComponent } from './auth/components/login/login.component';
 import { UploadFileListComponent } from './upload-file-list/upload-file-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UploadfilesComponent } from './uploadfiles/uploadfiles.component';
+import { SettingsComponent } from './auth/components/settings/settings.component';
+import { LoginComponent } from './auth/components/login/login.component';
 import { ProfileComponent } from './auth/components/profile/profile.component';
 
 const routes: Routes = [
@@ -22,6 +23,10 @@ const routes: Routes = [
   {
     path:'profile',
     component:ProfileComponent
+  },
+  {
+    path:'settings',
+    component:SettingsComponent
   },
 
 ];
