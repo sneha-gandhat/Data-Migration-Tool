56b1deaa24a5a25e08fafcb75aff9ba81b6857aa
Sneha Gandhat <sneha_gandhat@persistent.com>
2020-11-03 18:03:40 +0530

    Changes for Invalid Character rectification

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { SettingsComponent } from './components/settings/settings.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -5,18 +6,22 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, ProfileComponent],
+  declarations: [LoginComponent, RegisterComponent, ProfileComponent, SettingsComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule,
+    FormsModule
   ],
   //to give access to the components of this module to anyone who uses this
-  exports:[
-    LoginComponent, RegisterComponent,ProfileComponent
+  exports: [
+    LoginComponent, RegisterComponent, ProfileComponent, SettingsComponent
   ]
 })
 export class AuthModule { }
--- src/app/auth/components/settings/settings.component.ts
+++ src/app/auth/components/settings/settings.component.ts
@@ -6,10 +6,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  invalidCharacterList: string[] = ['!', '@', '#', '$', '%', '^', '&', '*'];
+  tempInvalidCharacterList: string[];
+  selectable = true;
+  removable = true;
+  isNewCharAdd: boolean = false;
+  newChar: string;
+  newReplacedChar: string;
+  newReplacedMultipleChar: string;
+  selectedChar: string = " ";
+  Flag1: boolean = false;
+  Flag2: boolean = false;
 
-  constructor() { }
+  constructor() {
+
+    this.tempInvalidCharacterList = this.invalidCharacterList;
+    console.log("this.tempInvalidCharacterList....."+this.tempInvalidCharacterList);
+  }
 
   ngOnInit(): void {
   }
 
+  expandToAddInvalidChar() {
+    this.isNewCharAdd = true;
+  }
+
+  //Show Single Char replacement Div on click of radio button
+  enableSingleCharDiv() {
+    this.Flag1 = true;
+    this.Flag2 = false;
+  }
+
+  //Show Multiple Char replacement Div on click of radio button
+  enableMultipleCharDiv() {
+    this.Flag2 = true;
+    this.Flag1 = false;
+  }
+
+  //Hide Div on click of radio button
+  disableDiv() {
+    this.Flag1 = false;
+    this.Flag2 = false;
+  }
+
+  addNewInvalidChar() {
+    this.invalidCharacterList.push(this.newChar);
+    this.newChar = " ";
+  }
+
+  replaceSingleNewChar() {
+
+  }
+
+  replaceMultipleNewChar(char: string) {
+    const index: number = this.tempInvalidCharacterList.indexOf(char);
+    if (index !== -1) {
+      this.tempInvalidCharacterList.splice(index, 1);
+    }
+    this.newReplacedMultipleChar = " ";
+  }
+
+  remove(char: string): void {
+    const index = this.invalidCharacterList.indexOf(char);
+
+    if (index >= 0) {
+      this.invalidCharacterList.splice(index, 1);
+    }
+  }
 }
